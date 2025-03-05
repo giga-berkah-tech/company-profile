8849572ef272ee109ce0af42ba043ceed6e5a3e6
yogiz <prayogiz@protonmail.com>
2025-03-05 14:56:03 +0700

    non index google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,86 +11,88 @@ import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://gigaberkahteknologi.com"),
-  title: {
-    template: "%s | PT. Giga Berkah Teknologi",
-    default: "PT. Giga Berkah Teknologi - Web & Mobile App Development Company",
-  },
-  description:
-    "Professional web and mobile app development services in Indonesia. Specializing in React, Next.js, Flutter, and native mobile development.",
-  keywords: [
-    "cheap web development service",
-    "cheap mobile app development service",
-    "affordable React development service",
-    "cheap Next.js Programmer Service",
-    "affordable Flutter Programmer Service",
-    "affordable Android development service",
-    "cheap iOS development service",
-    "Affordable IT solutions from Indonesia",
-    "software house Malang indonesia",
-    "PT Giga Berkah Teknologi",
-    "jasa pembuatan aplikasi mobile",
-    "jasa programmer murah",
-    "jasa pembuatan website modern",
-    "software house flutter, reactjs, nextjs",
-  ],
-  authors: [{ name: "PT. Giga Berkah Teknologi" }],
-  creator: "PT. Giga Berkah Teknologi",
-  publisher: "PT. Giga Berkah Teknologi",
-  formatDetection: {
-    email: false,
-    address: false,
-    telephone: false,
-  },
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    alternateLocale: "id_ID",
-    siteName: "PT. Giga Berkah Teknologi",
-    title: "PT. Giga Berkah Teknologi - Web & Mobile App Development Company",
-    description:
-      "Professional web and mobile app development services in Indonesia. Specializing in React, Next.js, Flutter, and native mobile development.",
-    images: [
-      {
-        url: "https://company.aplikasiku.web.id/_next/static/media/hero.6eece42f.png",
-        width: 1200,
-        height: 630,
-        alt: "Rent for Flutter & React programmer",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "PT. Giga Berkah Teknologi - Web & Mobile App Development Company",
-    description:
-      "Professional web and mobile app development services in Indonesia. Specializing in React, Next.js, Flutter, and native mobile development.",
-    images: [
-      "https://company.aplikasiku.web.id/_next/static/media/hero.6eece42f.png",
-    ],
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  verification: {
-    google: "qerVLV6teRmnE1ekPWcKVTZ211nPjrB4CeWlbk-S3UI",
-  },
-  alternates: {
-    canonical: "https://company.aplikasiku.web.id/",
-    languages: {
-      "en-US": "https://company.aplikasiku.web.id/?lang=en",
-      "id-ID": "https://company.aplikasiku.web.id/?lang=id",
-    },
-  },
-};
+// export const metadata: Metadata = {
+//   metadataBase: new URL("https://gigaberkahteknologi.com"),
+//   title: {
+//     template: "%s | PT. Giga Berkah Teknologi",
+//     default: "PT. Giga Berkah Teknologi - Web & Mobile App Development Company",
+//   },
+//   description:
+//     "Professional web and mobile app development services in Indonesia. Specializing in React, Next.js, Flutter, and native mobile development.",
+//   keywords: [
+//     "cheap web development service",
+//     "cheap mobile app development service",
+//     "affordable React development service",
+//     "cheap Next.js Programmer Service",
+//     "affordable Flutter Programmer Service",
+//     "affordable Android development service",
+//     "cheap iOS development service",
+//     "Affordable IT solutions from Indonesia",
+//     "software house Malang indonesia",
+//     "PT Giga Berkah Teknologi",
+//     "jasa pembuatan aplikasi mobile",
+//     "jasa programmer murah",
+//     "jasa pembuatan website modern",
+//     "software house flutter, reactjs, nextjs",
+//   ],
+//   authors: [{ name: "PT. Giga Berkah Teknologi" }],
+//   creator: "PT. Giga Berkah Teknologi",
+//   publisher: "PT. Giga Berkah Teknologi",
+//   formatDetection: {
+//     email: false,
+//     address: false,
+//     telephone: false,
+//   },
+//   openGraph: {
+//     type: "website",
+//     locale: "en_US",
+//     alternateLocale: "id_ID",
+//     siteName: "PT. Giga Berkah Teknologi",
+//     title: "PT. Giga Berkah Teknologi - Web & Mobile App Development Company",
+//     description:
+//       "Professional web and mobile app development services in Indonesia. Specializing in React, Next.js, Flutter, and native mobile development.",
+//     images: [
+//       {
+//         url: "https://company.aplikasiku.web.id/_next/static/media/hero.6eece42f.png",
+//         width: 1200,
+//         height: 630,
+//         alt: "Rent for Flutter & React programmer",
+//       },
+//     ],
+//   },
+  
+//   twitter: {
+//     card: "summary_large_image",
+//     title: "PT. Giga Berkah Teknologi - Web & Mobile App Development Company",
+//     description:
+//       "Professional web and mobile app development services in Indonesia. Specializing in React, Next.js, Flutter, and native mobile development.",
+//     images: [
+//       "https://company.aplikasiku.web.id/_next/static/media/hero.6eece42f.png",
+//     ],
+//   },
+//   robots: {
+//     index: true,
+//     follow: true,
+//     googleBot: {
+//       index: true,
+//       follow: true,
+//       "max-video-preview": -1,
+//       "max-image-preview": "large",
+//       "max-snippet": -1,
+//     },
+//   },
+//   // verification: {
+//   //   google: "qerVLV6teRmnE1ekPWcKVTZ211nPjrB4CeWlbk-S3UI",
+//   // },
+  
+//   alternates: {
+//     canonical: "https://company.aplikasiku.web.id/",
+//     languages: {
+//       "en-US": "https://company.aplikasiku.web.id/?lang=en",
+//       "id-ID": "https://company.aplikasiku.web.id/?lang=id",
+//     },
+//   },
+// };
 
 export default function RootLayout({
   children,
@@ -99,6 +101,9 @@ export default function RootLayout({
 }>) {
   return (
     <html suppressHydrationWarning>
+      <head>
+        <meta name="robots" content="noindex" />
+      </head>
       <body className={`${inter.className} bg-slate-100 dark:bg-slate-950`}>
         <Suspense>
           <ThemeProvider attribute="class">
